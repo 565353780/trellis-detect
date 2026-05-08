e9b0b9afbe92face1059263766ef9595fc97de44
chLi <565353780@qq.com>
2026-05-08 14:12:19 +0800

    simplify pip install

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,19 +1,19 @@
 cd ..
-git clone --recurse-submodules https://github.com/microsoft/TRELLIS.git
-git clone https://github.com/EasternJournalist/utils3d.git
-git clone https://github.com/facebookresearch/xformers.git
-git clone https://github.com/Dao-AILab/flash-attention.git
-git clone https://github.com/NVlabs/nvdiffrast.git
-git clone --recurse-submodules https://github.com/JeffreyXiang/diffoctreerast.git
-git clone https://github.com/autonomousvision/mip-splatting.git
+git clone --depth 1 --recurse-submodules https://github.com/microsoft/TRELLIS.git
+git clone --depth 1 https://github.com/EasternJournalist/utils3d.git
+git clone --depth 1 -b v0.0.33 --recursive https://github.com/facebookresearch/xformers.git
+git clone --depth 1 -b v2.8.3 https://github.com/Dao-AILab/flash-attention.git
+git clone --depth 1 https://github.com/NVlabs/nvdiffrast.git
+git clone --depth 1 --recurse-submodules https://github.com/JeffreyXiang/diffoctreerast.git
+git clone --depth 1 https://github.com/autonomousvision/mip-splatting.git
 # git clone https://github.com/FindDefinition/cumm.git
 # git clone https://github.com/traveller59/spconv.git
 git clone git@github.com:565353780/dino-v2-detect.git
 
 conda install -c conda-forge libstdcxx-ng libjpeg-turbo -y
 
-pip install torch==2.8.0 torchvision==0.23.0 torchaudio==2.8.0 \
-  --index-url https://download.pytorch.org/whl/cu128
+pip3 install torch torchvision torchaudio \
+  --index-url https://download.pytorch.org/whl/cu124
 
 pip install imageio imageio-ffmpeg tqdm easydict \
   opencv-python-headless scipy ninja rembg onnxruntime \
@@ -24,31 +24,19 @@ cd utils3d
 git checkout 9a4eb15e4021b67b12c460c7057d642626897ec8
 cd ..
 
-pip install ./utils3d/
-
-cd xformers
-git checkout v0.0.33
-git submodule update --init --recursive
-python setup.py install
-
-cd ../flash-attention
-git checkout v2.8.3
-python setup.py install
-
-cd ../nvdiffrast
-python setup.py install
-
-cd ../mip-splatting/submodules/diff-gaussian-rasterization/
-python setup.py install
-cd ../../..
-
-pip install ./diffoctreerast/
+pip install ./utils3d --no-build-isolation
+pip install ./xformers --no-build-isolation
+pip install ./flash-attention --no-build-isolation
+pip install ./nvdiffrast --no-build-isolation
+pip install ./mip-splatting/submodules/diff-gaussian-rasterization --no-build-isolation
+pip install ./diffoctreerast --no-build-isolation
 # cp -r ./extensions/vox2seq ./vox2seq
 # pip install ./vox2seq
 # pip install ./cumm
 # SPCONV_DISABLE_JIT=1 pip install ./spconv
 pip install spconv-cu120
 
-pip install kaolin==0.18.0 -f https://nvidia-kaolin.s3.us-east-2.amazonaws.com/torch-2.8.0_cu128.html
+pip install kaolin==0.18.0 \
+  -f https://nvidia-kaolin.s3.us-east-2.amazonaws.com/torch-2.6.0_cu124.html
 
 pip install gradio==4.44.1 gradio_litmodel3d==0.0.1
